7aff1644fe3322ccd2f7802dbeba2affecb5efcd
Roboto <aw13@vt.edu>
2018-01-12 15:15:56 -0500

    Working Brute Force Model!

--- BruteForce.py
+++ BruteForce.py
@@ -4,18 +4,30 @@ import numpy as np
 import pdb
 import time
 
+# Start Timer
+startTime = time.time()
 # Sample sudoku
 
-CountSudoku = np.array([[0, 0, 6, 0, 0,  8, 5, 0, 0],
-                       [0, 0, 0, 0, 7, 0, 6, 1, 3],
-                       [0, 0, 0, 0, 0, 0, 0, 0, 9],
-                       [0, 0, 0, 0, 9, 0, 0, 0, 1],
-                       [0, 0, 1, 0, 0, 0, 8, 0, 0],
-                       [4, 0, 0, 5, 3, 0, 0, 0, 0],
-                       [1, 0, 7, 0, 5, 3, 0, 0, 0],
-                       [0, 5, 0, 0, 6, 4, 0, 0, 0],
-                       [3, 0, 0, 1, 0, 0, 0, 6, 0]])
+# CountSudoku = np.array([[0, 0, 6, 0, 0, 8, 5, 0, 0],
+#                         [0, 0, 0, 0, 7, 0, 6, 1, 3],
+#                         [0, 0, 0, 0, 0, 0, 0, 0, 9],
+#                         [0, 0, 0, 0, 9, 0, 0, 0, 1],
+#                         [0, 0, 1, 0, 0, 0, 8, 0, 0],
+#                         [4, 0, 0, 5, 3, 0, 0, 0, 0],
+#                         [1, 0, 7, 0, 5, 3, 0, 0, 0],
+#                         [0, 5, 0, 0, 6, 4, 0, 0, 0],
+#                         [3, 0, 0, 1, 0, 0, 0, 6, 0]])
 
+# Impossible Puzzleeeee
+impossible = np.array([[0, 0, 0, 0, 0, 5, 0, 8, 0],
+                       [0, 0, 0, 6, 0, 1, 0, 4, 3],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 1, 0, 5, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 6, 0, 0, 0],
+                       [3, 0, 0, 0, 0, 0, 0, 0, 5],
+                       [5, 3, 0, 0, 0, 0, 0, 6, 1],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 4],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
 # Brute force solver
 
@@ -50,33 +62,43 @@ def Sudone(mat_in):
     row = 0
     m = 1 # momentum
     while done is 0:
-        while (CheckForDubs(sudoku) !=0) and sudoku[row, col]<9 :
+        while (CheckForDubs(sudoku) !=0 or m != 0) and sudoku[row, col]<9 :
             if mat_in[row,col]==0:
                 sudoku[row, col] +=1 # add one to cell number
-                m = 1
-        if CheckForDubs(sudoku) !=0:
+                m = 0
+            else:
+                break
+
+        if CheckForDubs(sudoku) !=0 or m == -1:
             if mat_in[row,col]==0:
                 sudoku[row, col] = 0 # clear cell
             col -= 1 # move back one cell
             m = -1
-            if col == 0:
-                col = 8
-                row -=1
-        if m=1:
+
+        if m >= 0:
             col +=1 # move forward one cell
             m = 1
-            if col == 9:
-               col = 0 # new row
-               row += 1 # move down one cell
+
+
+        if col == -1:
+            col = 8  # move backwards up a row
+            row -=1  # move to next row
+        elif col == 9:
+            col = 0    # new row
+            row += 1   # move down one cell
 
 
 
         print(sudoku, end="\r", flush=True)
         # time.sleep(.1)
 
-        done = True*((col == 9 and row == 9) or (col <= 0 and row <= 0))
+        # done = True*((col == 9 and row == 9) or (col == 0 and row < 0))
+        done = True*(row > 8 or row < 0)
+
     return sudoku
 
 # pdb.set_trace()
-X = Sudone(CountSudoku)
+X = Sudone(impossible)
 print(X)
+print('CheckForDubs(solution) =', CheckForDubs(X))
+print("--- %s seconds ---" % (time.time() - startTime))
