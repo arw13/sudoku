d35c95692c7c2ee52e203030c2a9e3481a7fdbd9
Roboto <aw13@vt.edu>
2018-02-03 12:37:17 -0500

    list insertion operations

--- linkedlists.py
+++ linkedlists.py
@@ -15,57 +15,53 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # This function prints contents of linked list
+    # starting from head
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data,)
+            temp = temp.next
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        # 1 & 2: Allocate the Node &
+        #        Put in the data
+        new_node = Node(new_data)
+        # 3. Make next of new Node as head
+        new_node.next = self.head
+        # 4. Move the head to point to new Node
+        self.head = new_node
+
+    # Inserts a new node after the given prev_node.
+    def insertAfter(self, prev_node, new_data):
+    # 1. check if the given prev_node exists
+    if prev_node is None:
+        print "The given previous node must inLinkedList."
+        return
+    #  2. Create new node &
+    #  3. Put in the data
+    new_node = Node(new_data)
+    # 4. Make next of new Node as next of prev_node
+    new_node.next = prev_node.next
+    # 5. make next of prev_node as new_node
+    prev_node.next = new_node
+
+
 # main run
 if __name__ == '__main__':
 
         # Initialize list head
-        linkList = Node(1)
+        linkList = LinkedList()
 
         # Define head of List
         linkList.head = Node(1)
         # Make two more nodes
-
         scdNode = Node(2)
         thdNode = Node(3)
 
-        '''
-        from https://www.geeksforgeeks.org/linked-list-set-1-introduction
-
-        Three nodes have been created.
-        We have references to these three blocks as first,
-        second and third
-
-        llist.head        second              third
-             |                |                  |
-             |                |                  |
-        +----+------+     +----+------+     +----+------+
-        | 1  | None |     | 2  | None |     |  3 | None |
-        +----+------+     +----+------+     +----+------+
-        '''
-
         # Link Nodes
         linkList.head.next = scdNode
-        '''
-        Now next of first Node refers to second.  So they
-        both are linked.
-
-        llist.head        second              third
-             |                |                  |
-             |                |                  |
-        +----+------+     +----+------+     +----+------+
-        | 1  |  o-------->| 2  | null |     |  3 | null |
-        +----+------+     +----+------+     +----+------+
-        '''
         scdNode.next = thdNode
 
-        '''
-        Now next of second Node refers to third.  So all three
-        nodes are linked.
-
-        llist.head        second              third
-         |                |                  |
-         |                |                  |
-        +----+------+     +----+------+     +----+------+
-        | 1  |  o-------->| 2  |  o-------->|  3 | null |
-        +----+------+     +----+------+     +----+------+
-        '''
+        linkList.printList()
